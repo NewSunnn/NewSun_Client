9ca747e4b884803aa08c475671132acbf8d9e5e3
sayyyho <323psh@naver.com>
2024-03-14 18:53:18 +0900

    Chore: package manager 등 수정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,7 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import {fileURLToPath} from 'url';
-/**
- * url 부분 ?
- */
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
